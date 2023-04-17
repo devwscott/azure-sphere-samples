cf8e365a6dd2f8ad49c5c9812c0817390de7dfd4
dev <dev@dev.com>
2023-04-17 11:35:03 +0900

    [MqttTLS] 1st commit - copy from MQTT(non TLS)

---
+++ Samples/MqttTLS/MqttTLS_HighLevelApp/udplog.h
@@ -0,0 +1,8 @@
+/* Copyright (c) Microsoft Corporation. All rights reserved.
+   Licensed under the MIT License. */
+
+#pragma once
+
+// int Log_Debug(const char *fmt, ...);
+int Udp_Debug(const char *fmt, ...);
+
