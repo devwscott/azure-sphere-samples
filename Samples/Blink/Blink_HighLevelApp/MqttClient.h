d88387a5a0ceab026189d44ef6316e3a382f1ad8
dev <dev@dev.com>
2023-05-16 10:57:14 +0900

    [SC][MqttClient] added the connect() without CA Cert, disConnect()

--- Samples/Blink/Blink_HighLevelApp/MqttClient.h
+++ Samples/Blink/Blink_HighLevelApp/MqttClient.h
@@ -3,6 +3,8 @@
 #include <pthread.h>
 #include <string>
 
+#include "mqtt.h"
+
 using namespace std;
 
 class IMqttClientListener{
@@ -18,15 +20,24 @@ private:
     IMqttClientListener *m_listener;
     bool m_running;
     pthread_t m_thread;
-
     static void* __syncThread__(void *arg);
+
+    int m_socketFd;
+    // void *m_client;
+    mqtt_client m_client;
+    unsigned char m_sendbuf[512];
+    unsigned char m_recvbuf[512];
+    int _open_nb_socket(const string &addr, const string &port);
+    static void __subscribe_callback__(void **arg, struct mqtt_response_publish* published);
 
 public:
     MqttClient(IMqttClientListener *callback);
     virtual ~MqttClient();
 
     bool init(int qosLevel, int keepAliveTime);
+    bool connect(const string &broker, const string &port);
     bool connect(const string &broker, const string &port, const string &certFile);
+    bool disConnect();
     bool subscribe(string &topic);
     bool unSubscribe(string &topic);
     bool publish(string &topic, string &msg);
