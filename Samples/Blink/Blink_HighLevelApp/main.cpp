6bf7883e71295824f84b56bacef099aefd0defd1
dev <dev@dev.com>
2023-05-22 09:33:21 +0900

    [main] added the map example

--- Samples/Blink/Blink_HighLevelApp/main.cpp
+++ Samples/Blink/Blink_HighLevelApp/main.cpp
@@ -5,6 +5,7 @@
 #include <hw/template_appliance.h>
 #include <cstdio>
 #include <new>
+#include <map>
 
 #include "Coordinator.h"
 
@@ -15,6 +16,17 @@ extern "C" void __cxa_pure_virtual() { while (1); }
 
 int main(int argc, char *argv[])
 {
+    // map<string, int> myMap;
+
+    // myMap["Alice"] = 25;
+    // myMap["Bob"] = 31;
+    // myMap["Charlie"] = 45;
+
+    // Log_Debug("--%d\n", myMap["Alice"]);
+    // Log_Debug("--%d\n", myMap["Bob"]);
+    // Log_Debug("--%d\n", myMap["Charlie"]);
+    
+
     // Blink blink(TEMPLATE_LED);
     Blink *blink = new Blink(TEMPLATE_LED, "green");
     blink->start(4000);
@@ -28,7 +40,6 @@ int main(int argc, char *argv[])
 
     Log_Debug("Blink CPP Example start!!\n");
     Log_Debug("Color is %s\n", blink->getColor().c_str());
-
 
     while(1){
         //
