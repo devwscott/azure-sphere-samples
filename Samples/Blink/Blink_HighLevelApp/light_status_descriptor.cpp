b08e2100d109823e0be2f1a0d2c2728d478e822f
dev <dev@dev.com>
2023-07-04 11:02:17 +0900

    [LightStatusDescriptor] added the TEST codes for LightStatusDescriptor

--- Samples/Blink/Blink_HighLevelApp/light_status_descriptor.cpp
+++ Samples/Blink/Blink_HighLevelApp/light_status_descriptor.cpp
@@ -97,3 +97,100 @@ void LightStatusDescriptor::updateDescriptorLength(){
 }
 
 
+/*  TEST codes for LightStatusDescriptor which should be located at main.cpp
+
+#include "BitReadWriter.h"
+#include "descriptor.h"
+#include "light_status_descriptor.h"
+
+    unsigned char buff_data[] = {0x47, 0x01, 0xb0, 0x0c, 0x52, 0x54, 0xd5, 0xff, 0xff, 0x00, 0x01, 0x1f, 0x12, 0x34, 0x56, 0x78};
+    int value;
+
+    unsigned char* buff;
+    buff = new unsigned char[20];
+    
+    memcpy(buff, buff_data, sizeof(buff_data));
+
+    BitReadWriter *bitRW = new BitReadWriter();
+    bitRW->SetBuffer(buff);
+
+    value = bitRW->Read_On_Buffer(8);
+    Log_Debug("magic : 0x%x\n", value);
+
+    value = bitRW->Read_On_Buffer(8);
+    Log_Debug("table_id : 0x%x\n", value);
+
+    value = bitRW->Read_On_Buffer(1);
+    Log_Debug("section_syntax : 0x%x\n", value);
+
+    bitRW->Skip_On_Buffer(3);
+
+    value = bitRW->Read_On_Buffer(12);
+    Log_Debug("section_length : 0x%x\n", value);
+
+    value = bitRW->Read_On_Buffer(16);
+    Log_Debug("coreid : 0x%x\n", value);
+    Log_Debug("coreid : %c%c\n", (value&0xFF00)>>8, value&0xFF);
+
+    bitRW->Skip_On_Buffer(2);
+
+    value = bitRW->Read_On_Buffer(5);
+    Log_Debug("version : 0x%x\n", value);
+
+    bitRW->Skip_On_Buffer(17);
+
+    // LightStatusDescriptor* desc = new LightStatusDescriptor(bitRW);
+    LightStatusDescriptor* desc = new LightStatusDescriptor();
+
+    desc->SetDeviceStatus(3, 0);
+
+    value = desc->GetDescriptorLength();
+    Log_Debug("desc.length : 0x%x\n", value);
+
+    desc->WriteDescriptor(bitRW);
+
+    desc->PrintDescriptor();
+
+    value = desc->GetDescriptorTag();
+    Log_Debug("desc.tag : 0x%x\n", value);
+
+    value = desc->GetDescriptorLength();
+    Log_Debug("desc.length : 0x%x\n", value);
+
+    value = desc->GetDeviceCount();
+    Log_Debug("dev count : 0x%x\n", value);
+
+    value = desc->GetDeviceStatus(0);
+    Log_Debug("dev status : 0x%x\n", value);
+
+
+
+#if 0
+    value = bitRW->Read_On_Buffer(8);
+    Log_Debug("desc.tag : 0x%x\n", value);
+
+    value = bitRW->Read_On_Buffer(8);
+    Log_Debug("desc.length : 0x%x\n", value);
+
+
+    value = bitRW->Read_On_Buffer(4);
+    Log_Debug("device id : 0x%x\n", value);
+
+
+    bitRW->Skip_On_Buffer(3);
+
+    value = bitRW->Read_On_Buffer(1);
+    Log_Debug("device status : 0x%x\n", value);
+#endif
+
+
+    value = bitRW->Read_On_Buffer(32);
+    Log_Debug("CRC_32 : 0x%x\n", value);
+
+    unsigned char* buff2;
+    buff2 = bitRW->GetBufAdd();
+
+    for(int i=0; i<16; i++){
+        Log_Debug("[%d] 0x%02x\n", i, buff2[i]);
+    }
+*/
