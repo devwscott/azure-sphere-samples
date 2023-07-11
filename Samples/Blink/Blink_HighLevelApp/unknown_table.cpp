bc433674e4dede8b074d9b73478a4690441b0ed8
dev <dev@dev.com>
2023-07-11 12:25:37 +0900

    [UnknownTable] added the test codes for UnknownTable class

--- Samples/Blink/Blink_HighLevelApp/unknown_table.cpp
+++ Samples/Blink/Blink_HighLevelApp/unknown_table.cpp
@@ -30,3 +30,33 @@ void UnknownTable::__decode_table_body__(){
 
 }
 
+
+/* Test Code for UnknownTable
+#include "unknown_table.h"
+
+    // Light control
+    unsigned char buff_data[] = {0x47, 0x01, 0xb0, 0x0c, 0x52, 0x54, 0xd5, 0xff, 0xff, 0x00, 0x01, 0x1f, 0x12, 0x34, 0x56, 0x78};
+    // Light Status
+    // unsigned char buff_data[] = {0x47, 0x01, 0xb0, 0x0c, 0x52, 0x54, 0xd5, 0xff, 0xff, 0x01, 0x01, 0x3f, 0x12, 0x34, 0x56, 0x78};
+
+    int value;
+
+    unsigned char* buff;
+    buff = new unsigned char[20];
+    
+    memcpy(buff, buff_data, sizeof(buff_data));
+
+    // Table* table = new Table(buff, sizeof(buff_data));
+    UnknownTable* table = new UnknownTable(buff, sizeof(buff_data));
+    table->PrintTable();
+    table->PrintTable("UnknownTable");
+    value = table->GetSectionLength();
+    Log_Debug("GetSectionLength : %d\n", value);
+
+    unsigned char* buff2;
+    buff2 = table->GetSection();
+    for(int i=0; i<16; i++){
+        Log_Debug("[%d] 0x%02x\n", i, buff2[i]);
+    }
+
+*/
